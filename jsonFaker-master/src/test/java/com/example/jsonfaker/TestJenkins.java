7574249344630fe600270a1bd23e08e026fc9520
Dunivivi <89550560+Dunivivi@users.noreply.github.com>
2022-11-18 21:17:30 +0200

    test add

---
+++ jsonFaker-master/src/test/java/com/example/jsonfaker/TestJenkins.java
@@ -0,0 +1,24 @@
+package com.example.jsonfaker;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TestJenkins {
+
+@Test
+    void Test1() {
+        assertEquals(1,1);
+    }
+    @Test
+    void Test2() {
+        assertEquals(1,1);
+    }
+    @Test
+    void Test3() {
+        assertEquals(1,1);
+    }
+    @Test
+    void Test4() {
+        assertEquals(1,1);
+    }
+
+}
